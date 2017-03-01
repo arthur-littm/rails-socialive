16b413bd56476655bcc9053a4c14d05172dc35f7
theodore laurent <theodorelaurent@hotmail.com>
2017-03-01 16:25:19 +0000

    added logo to the home cards and added adventure and fitness category on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @adventure_livestreams = Livestream.all.where(category: 'adventure')
+    @fitness_livestreams = Livestream.all.where(category: 'fitness')
+    # add more categories when cards are complete #
   end
 end

15bcb1da774f7aa299fdcbb972dec2e6d873b191
Evan Thomas <evanhen@hotmail.co.uk>
2017-03-01 19:41:34 +0000

    fixed the ActionCable chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-
   mount ActionCable.server => '/cable'
-
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
   root to: 'pages#home'
-
   resources :livestreams
   get '/dashboard' => 'pages#dashboard'
-
-  resources :chat_rooms, only: [:new, :create, :show, :index]
-
-# This line should be at the end
-
   get '/:category' => 'livestreams#category_show'
-  # --
-
+  # resources :chat_rooms, only: [:new, :create, :show, :index]
 end

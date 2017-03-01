0d0223d405574761dbbaf158fc1c31105bf8b5e8
Evan Thomas <evanhen@hotmail.co.uk>
2017-03-01 18:27:27 +0000

    chat working

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,9 +4,13 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_room
 
+  default_scope {order(:created_at => :asc)}
+
   after_create_commit { MessageBroadcastJob.perform_later(self) }
 
   def timestamp
     created_at.strftime('%H:%M:%S %d %B %Y')
   end
+
+
 end

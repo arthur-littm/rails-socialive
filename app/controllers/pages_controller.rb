2617b7fa0a68e3103f07d15b6577d12911a39945
Remi dYvoire <remi.dyvoire@gmail.com>
2017-03-02 12:19:45 +0000

    added cloudinary

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,7 +20,7 @@ class PagesController < ApplicationController
   private
 
   def profile_params
-    params.require(:user).permit(:facebooklink, :twitterlink, :instagramlink, :youtubelink, :snapchatlink, :bio, :fav1title, :fav2title, :fav3title, :fav1, :fav2, :fav3)
+    params.require(:user).permit(:facebooklink, :twitterlink, :instagramlink, :youtubelink, :snapchatlink, :bio, :fav1title, :fav2title, :fav3title, :fav1, :fav2, :fav3, :profilepic, :images)
   end
 
 end

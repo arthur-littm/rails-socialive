51633428977bf06d9dba6b7e69b45e837573c9fd
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-03-02 16:39:26 +0000

    just jq

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,2 @@
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
-//= require jquery-fileupload/basic
-//= require cloudinary/jquery.cloudinary
-//= require attachinary
-//= require_tree .

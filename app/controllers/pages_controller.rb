8168d1cba62580d025791e911459bcc844c273d8
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-03-03 14:32:25 +0000

    following/follower added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,10 +17,22 @@ class PagesController < ApplicationController
     @user.update(profile_params)
   end
 
+  def following
+    @title = "Following"
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
 
   def profile_params
-    params.require(:user).permit(:facebooklink, :twitterlink, :instagramlink, :youtubelink, :snapchatlink, :bio, :fav1title, :fav2title, :fav3title, :fav1, :fav2, :fav3, :profilepic, :images)
+    params.require(:user).permit(:facebooklink, :twitterlink, :instagramlink, :youtubelink, :snapchatlink, :bio, :fav1title, :fav2title, :fav3title, :fav1, :fav2, :fav3, :profilepic, :images, :following, :followers)
   end
 
 end

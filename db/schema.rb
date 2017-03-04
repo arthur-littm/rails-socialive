93e29d484f8cfce66dfa586442b0c6abb01936e0
Evan Thomas <evanhen@hotmail.co.uk>
2017-03-04 22:14:17 +0000

    user gets random color on sign up which is then used in the chat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303110225) do
+ActiveRecord::Schema.define(version: 20170304215440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20170303110225) do
     t.text     "fav1"
     t.text     "fav2"
     t.text     "fav3"
+    t.string   "color"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end

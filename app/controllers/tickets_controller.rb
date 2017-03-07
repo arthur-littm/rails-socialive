12937c96f056fcfdf7bbb755151ac377ceddc662
Evan Thomas <evanhen@hotmail.co.uk>
2017-03-07 13:26:12 +0000

    ticket sold out logic implemented

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,7 +4,7 @@ class TicketsController < ApplicationController
     @livestream = Livestream.find(params[:livestream_id])
 
     #checks if user has enough rubies to buy ticket and debits/credits if he does
-    if current_user.balance >= @livestream.ticket_price
+    if current_user.balance >= @livestream.ticket_price && (@livestream.tickets_available - @livestream.tickets_sold) > 0
       current_user.balance -= @livestream.ticket_price
       @livestream.user.earned_balance += @livestream.ticket_price
       current_user.save
@@ -15,10 +15,12 @@ class TicketsController < ApplicationController
       @ticket.livestream = @livestream
       @ticket.save
       #updates Tickets sold in the livestream table
-      @livestream.tickets_sold -= 1
+      @livestream.tickets_sold += 1
       @livestream.save
       #redirects
       redirect_to livestream_path(@livestream)
+    elsif current_user.balance >= @livestream.ticket_price
+      redirect_to livestream_path(@livestream), alert: "Tickets are sold out"
     else
       redirect_to livestream_path(@livestream), alert: "You have insufficient funds"
     end

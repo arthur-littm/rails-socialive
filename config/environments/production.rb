adbcc64588048dede312d63a19974918fb2e2140
UgoMare <ugo.mare.epitech@gmail.com>
2017-03-09 13:08:43 +0100

    Update production link for wss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,11 +38,12 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.url = "wss://socialive-rails.herokuapp.com/cable"
+  config.action_cable.url = "wss://socialive.tv/cable"
 
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = ['https://socialive-rails.herokuapp.com',
-                                              'http://socialive-rails.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://socialive.tv',
+                                              'http://socialive.tv',
+                                            ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 

c37fed2ce169725314c521dbf3fe48a14e2ed251
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-03-10 15:12:01 +0000

    fix prices

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,9 @@
 class Order < ApplicationRecord
 
   PACKS = {
-    pack1: { rubies: 100, price: 10 },
-    pack2: { rubies: 200, price: 20 },
-    pack3: { rubies: 500, price: 40 },
+    pack1: { rubies: 100, price: 5 },
+    pack2: { rubies: 200, price: 10 },
+    pack3: { rubies: 500, price: 20 },
   }
 
   monetize :amount_pennies  # or :amount_pennies

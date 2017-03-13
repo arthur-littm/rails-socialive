5400bd07cf6b8e5f6dd27cb19e13f8d4953f1ab2
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-03-13 17:07:36 +0000

    added livestream model to active admin

---
+++ app/admin/livestream.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Livestream do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+  permit_params :main_picture, :hour_of_stream, :tickets_available, :category, :user_id, :title, :ticket_price, :description
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
+

c5478df9a86be3428aa6ebc64a36d76299687b55
Remi dYvoire <remi.dyvoire@gmail.com>
2017-03-21 00:28:47 +0000

    arrow in shop changing class

--- app/assets/javascripts/shop.js
+++ app/assets/javascripts/shop.js
@@ -5,12 +5,25 @@
   // });
 
 function shopFunction() {
-    var x = document.getElementById('otherproducts');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
+
+  var div = document.getElementById("shopbtndiv");
+
+  console.log(div.class)
+  if (div.class === 'rotated-image') {
+      div.class = 'rotated-image2';
+  }
+  else {
+      div.class = 'rotated-image';
+  }
+  console.log(div.class)
+
+
+  var x = document.getElementById('otherproducts');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
 }
 
 // });

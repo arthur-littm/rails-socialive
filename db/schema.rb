f04325aba26f53feaa3b3a9c182cd7acf686919d
theodorelaurent <theodorelaurent@hotmail.com>
2017-06-23 12:09:46 +0200

    added msgtokbox as string in messages table, added ev's callback code for the tokboxchat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614140552) do
+ActiveRecord::Schema.define(version: 20170623100650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20170614140552) do
     t.datetime "updated_at",    null: false
     t.integer  "livestream_id"
     t.boolean  "donation"
+    t.string   "msgtokbox"
     t.index ["livestream_id"], name: "index_messages_on_livestream_id", using: :btree
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end

d72df54f3b5142ee091f1ef573f16be2130a0749
theodorelaurent <theodorelaurent@hotmail.com>
2017-07-25 17:32:04 +0100

    added a jquesry function that prevents a user from purchasing multiple tickets

---
+++ app/assets/javascripts/buy_ticket.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){
+//   $(".btn-buy-ticket").click(function(){
+//     $(".fa-for-js").removeClass('fa-ticket');
+//     $(".fa-for-js").addClass("fa-spinner fa-spin");
+//   });
+// });
